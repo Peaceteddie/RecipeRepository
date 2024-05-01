9e58e14982a21cbd2dba85efda862db5d7e75364
Peaceteddie <Peaceteddie@gmail.com>
2024-05-01 19:47:16 +0200

    Added link to recipe page and did some refactoring

---
+++ Models/User.cs
@@ -0,0 +1,7 @@
+namespace RecipeRepository;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Username { get; set; }
+}
--- RecipeRepository.cs
+++ RecipeRepository.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
 namespace RecipeRepository;
 public class RecipeDbContext : DbContext
 {
@@ -6,15 +8,27 @@ public class RecipeDbContext : DbContext
     public DbSet<Ingredient> Ingredients { get; set; }
     public DbSet<RecipeImage> RecipeImages { get; set; }
     public DbSet<SeasonalIngredient> SeasonalIngredients { get; set; }
-    public async Task<List<Recipe>> GetRecipesWithIngredients()
-    => await Recipes.Include(r => r.Ingredients).Include(r => r.Image).ToListAsync();
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        string host = DotNetEnv.Env.GetString("POSTGRES_HOST");
-        string port = DotNetEnv.Env.GetString("POSTGRES_PORT");
-        string database = DotNetEnv.Env.GetString("POSTGRES_DB");
-        string username = DotNetEnv.Env.GetString("POSTGRES_USER");
-        string password = DotNetEnv.Env.GetString("POSTGRES_PASSWORD");
+        string host = Env.GetString("POSTGRES_HOST");
+        string port = Env.GetString("POSTGRES_PORT");
+        string database = Env.GetString("POSTGRES_DB");
+        string username = Env.GetString("POSTGRES_USER");
+        string password = Env.GetString("POSTGRES_PASSWORD");
+        optionsBuilder.UseNpgsql($"Host={host};Port={port};Database={database};Username={username};Password={password}");
+    }
+}
+
+public class AuthDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string host = Env.GetString("MYSQL_HOST");
+        string port = Env.GetString("MYSQL_PORT");
+        string database = Env.GetString("MYSQL_DB");
+        string username = Env.GetString("MYSQL_USER");
+        string password = Env.GetString("MYSQL_PASSWORD");
         optionsBuilder.UseNpgsql($"Host={host};Port={port};Database={database};Username={username};Password={password}");
     }
 }
